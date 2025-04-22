24b1ef698a43c4c8dd2a34ad2ffcfa5a5c8249a5
matjazo18 <matjaz.gazvoda@gmail.com>
2025-04-22 19:46:31 +0200

    text-animation

--- app/component/Animation.js
+++ app/component/Animation.js
@@ -1,19 +1,47 @@
-import { motion, useScroll } from "framer-motion";
+"use client";
+import {
+  motion,
+  useAnimation,
+  useInView,
+  useScroll,
+  useTransform,
+} from "framer-motion";
+import { use, useEffect, useRef } from "react";
 
 export default function Animation() {
   const ContainerVariants = {
     hidden: { opacity: 0 },
     show: { opacity: 1, transition: { staggerChildren: 0.25 } },
   };
-  const { scrollYProgress, completionProgress } = useScroll();
+  const { completionProgress } = useScroll();
 
   const gridVairiants = { hidden: { opacity: 0 }, show: { opacity: 1 } };
   const svgIcons = {
     hidden: { opacity: 0, pathLength: 0, fill: "rgba(252,211,77,0)" },
     visible: { opacity: 1, pathLength: 1, fill: "rgba(252,211,77,1)" },
   };
+
+  const containerREf = useRef(null);
+
+  // this assure that it can only be visable at that point
+  const isInView = useInView(containerREf, { once: true });
+  const mainControls = useAnimation();
+
+  const { scrollYProgress } = useScroll({
+    target: containerREf,
+    offset: ["start end", "end end"],
+  });
+
+  const paragraphOne = useTransform(scrollYProgress, [0, 1], ["-100%", "0%"]);
+  const paragraphTwo = useTransform(scrollYProgress, [0, 1], ["100%", "0%"]);
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start("visible");
+    }
+  }, [isInView]);
+
   return (
-    <div className="flex flex-col gap-10 overflow-x-hidden">
+    <div className="flex flex-col gap-10 overflow-x-hidden justify-center text-center">
       <motion.section
         variants={ContainerVariants}
         initial="hidden"
@@ -142,6 +170,56 @@ export default function Animation() {
           </motion.svg>
         </motion.div>
       </motion.section>
+
+      <section
+        className=" flex flex-col items-center gap-10 mb-10 max-w-3xl mx-auto justify-center text-center"
+        ref={containerREf}
+      >
+        <div className="flex flex-col justify-center text-center">
+          <motion.h1
+            className=" text-5xl tracking-wide text-slate-800 text-center"
+            animate={mainControls}
+            initial="hidden"
+            variants={{
+              hidden: { opacity: 0, y: 75 },
+              visible: { opacity: 1, y: 0 },
+            }}
+            transition={{ delay: 0.3 }}
+          >
+            Just keep scrolling
+          </motion.h1>
+          <motion.p
+            className="text-slate-800 text-center  text-2xl tracking-wide"
+            animate={mainControls}
+            initial="hidden"
+            variants={{
+              hidden: { opacity: 0, x: -100 },
+              visible: { opacity: 1, x: 0 },
+            }}
+            transition={{ delay: 0.5 }}
+            style={{ x: paragraphOne }}
+          >
+            Today it was a very hard day at scholl but also very fun one. First
+            I got my grades back from psyhology and I got a 2 50% to be exact.
+            And I had this luck that she will sum it to 4 as the end Grade
+          </motion.p>
+          <motion.p
+            className="text-slate-800 text-center  text-2xl tracking-wide"
+            animate={mainControls}
+            initial="hidden"
+            variants={{
+              hidden: { opacity: 0, x: 100 },
+              visible: { opacity: 1, x: 0 },
+            }}
+            transition={{ delay: 1 }}
+            style={{ x: paragraphTwo }}
+          >
+            Today it was a very hard day at scholl but also very fun one. First
+            I got my grades back from psyhology and I got a 2 50% to be exact.
+            And I had this luck that she will sum it to 4 as the end Grade
+          </motion.p>
+        </div>
+      </section>
     </div>
   );
 }
