5343c1bbd886aecf28c5b8f147e4593af11db37c
matjazo18 <matjaz.gazvoda@gmail.com>
2025-06-14 12:58:35 +0200

    Pricing section

--- app/component/InterviewQuestions.js
+++ app/component/InterviewQuestions.js
@@ -38,7 +38,7 @@ export default function InterviewQuestions({ filterElements }) {
         </div>
       </div>
       <div className="flex justify-center gap-4 items-center mt-20 rounded-lg">
-        <div className="w-[350px] h-[450px] bg-white outline outline-2 outline-customOrange rounded-3xl shadow-xl">
+        <div className="w-[350px] h-[465px] bg-white outline outline-2 outline-customOrange rounded-3xl shadow-xl">
           <div className="flex flex-col justify-start items-start p-8 space-y-6 h-full">
             <div className="space-y-3">
               <h1 className="font-sans text-black font-extrabold text-3xl leading-snug">
@@ -50,10 +50,68 @@ export default function InterviewQuestions({ filterElements }) {
                 free!
               </p>
             </div>
+            <ul className="mt-4 space-y-2 text-sm text-gray-700">
+              <li className="flex items-center gap-2">
+                <span className="text-green-400 text-lg">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="size-5"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </span>{" "}
+                Personal google sheet
+              </li>
+              <li className="flex items-center gap-2">
+                <span className="text-red-400 text-lg">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="size-5"
+                  >
+                    <path d="M6.28 5.22a.75.75 0 0 0-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 1 0 1.06 1.06L10 11.06l3.72 3.72a.75.75 0 1 0 1.06-1.06L11.06 10l3.72-3.72a.75.75 0 0 0-1.06-1.06L10 8.94 6.28 5.22Z" />
+                  </svg>
+                </span>{" "}
+                Custom Dashboard
+              </li>
+              <li className="flex items-center gap-2">
+                <span className="text-red-400 text-lg">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="size-5"
+                  >
+                    <path d="M6.28 5.22a.75.75 0 0 0-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 1 0 1.06 1.06L10 11.06l3.72 3.72a.75.75 0 1 0 1.06-1.06L11.06 10l3.72-3.72a.75.75 0 0 0-1.06-1.06L10 8.94 6.28 5.22Z" />
+                  </svg>
+                </span>{" "}
+                AI Analysis
+              </li>
+              <li className="flex items-center gap-2">
+                <span className="text-red-400 text-lg">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="size-5"
+                  >
+                    <path d="M6.28 5.22a.75.75 0 0 0-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 1 0 1.06 1.06L10 11.06l3.72 3.72a.75.75 0 1 0 1.06-1.06L11.06 10l3.72-3.72a.75.75 0 0 0-1.06-1.06L10 8.94 6.28 5.22Z" />
+                  </svg>
+                </span>{" "}
+                Investing Section
+              </li>
+            </ul>
 
             <form
               onSubmit={(e) => e.preventDefault()}
-              className="w-full space-y-4"
+              className="w-full space-y-5"
             >
               <input
                 type="email"
@@ -68,7 +126,7 @@ export default function InterviewQuestions({ filterElements }) {
 
               <button
                 onClick={handleCopyEmail}
-                className="bg-customOrange hover:bg-orange-600 text-white font-semibold py-2 px-6 rounded-xl transition duration-300 w-full"
+                className="bg-customOrange hover:bg-orange-600 text-white font-semibold py-2  px-6 rounded-xl transition duration-300 w-full "
                 disabled={loading}
               >
                 <div className="flex items-center justify-center space-x-2">
@@ -83,38 +141,12 @@ export default function InterviewQuestions({ filterElements }) {
                 </div>
               </button>
             </form>
-            <div className="flex flex-col items-center justify-center gap-2">
-              <p className="font-sans text-gray-600 text-base">
-                <span className="font-extrabold">JOIN</span>
-                <span className="font-extrabold text-customOrange">
-                  {" "}
-                  1163
-                </span>{" "}
-                users tracking their finances
-              </p>
-              <div className="justy-start avatar-group -space-x-3 flex">
-                {avatarUrls.map((src, index) => (
-                  <div
-                    key={index}
-                    className="avatar h-9 w-9 border-[2px] rounded-full overflow-hidden"
-                  >
-                    <Image
-                      src={src}
-                      alt={`Avatar ${index + 1}`}
-                      width={36}
-                      height={36}
-                      className="w-full h-full object-cover"
-                    />
-                  </div>
-                ))}
-              </div>
-            </div>
           </div>
         </div>
 
-        <div className="relative w-[350px] h-[450px] bg-gradient-to-b from-black to-gray-900 outline outline-2 outline-customOrange rounded-3xl shadow-xl">
+        <div className="relative w-[350px] h-[465px] bg-gradient-to-b from-black to-gray-900 outline outline-2 outline-customOrange rounded-3xl shadow-xl">
           {/* This badge is now positioned relative to the card */}
-          <div className="absolute top-12 right-8 bg-white py-1.5 px-4 rounded-3xl flex items-center justify-center shadow-md">
+          <div className="absolute top-12 right-8 bg-white py-1.5 px-4 rounded-3xl flex items-center justify-center shadow-md outline outline-customOrange">
             <p className="text-sm text-customOrange font-semibold">
               🔥 Save 51%
             </p>
@@ -137,15 +169,72 @@ export default function InterviewQuestions({ filterElements }) {
             {/* Features */}
             <ul className="mt-4 space-y-2 text-sm text-gray-300">
               <li className="flex items-center gap-2">
-                <span className="text-green-400 text-lg">✔</span> Custom
-                Dashboard
+                <span className="text-green-400 text-lg">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="size-5"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </span>{" "}
+                Personal google sheet
               </li>
               <li className="flex items-center gap-2">
-                <span className="text-green-400 text-lg">✔</span> AI Analysis
+                <span className="text-green-400 text-lg">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="size-5"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </span>{" "}
+                Custom Dashboard
               </li>
               <li className="flex items-center gap-2">
-                <span className="text-green-400 text-lg">✔</span> Investing
-                Section
+                <span className="text-green-400 text-lg">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="size-5"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </span>{" "}
+                AI Analysis
+              </li>
+              <li className="flex items-center gap-2">
+                <span className="text-green-400 text-lg">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="size-5"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </span>{" "}
+                Investing Section
               </li>
             </ul>
 
@@ -160,6 +249,29 @@ export default function InterviewQuestions({ filterElements }) {
               Buy Now
             </button>
           </div>
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-center gap-2 mt-8">
+        <p className="font-sans text-gray-600 text-base">
+          <span className="font-extrabold">JOIN</span>
+          <span className="font-extrabold text-customOrange"> 1163</span> users
+          tracking their finances
+        </p>
+        <div className="justy-start avatar-group -space-x-3 flex">
+          {avatarUrls.map((src, index) => (
+            <div
+              key={index}
+              className="avatar h-9 w-9 border-[2px] rounded-full overflow-hidden"
+            >
+              <Image
+                src={src}
+                alt={`Avatar ${index + 1}`}
+                width={36}
+                height={36}
+                className="w-full h-full object-cover"
+              />
+            </div>
+          ))}
         </div>
       </div>
     </div>
